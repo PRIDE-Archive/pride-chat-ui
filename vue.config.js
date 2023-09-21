594d8cf566462c7a947b8f338d936f34002a9ef6
shabai517 <shabai517@gmail.com>
2023-09-21 10:32:06 +0100

    update deploy URL

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: process.env.NODE_ENV === "production" ? "/chat-ui-public/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/pride/chatbot/" : "/",
   productionSourceMap: false,
 });
