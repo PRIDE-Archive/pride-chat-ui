c480be68d9cd365dde9d800dd1ae4f320fdc7f61
shabai517 <shabai517@gmail.com>
2023-11-20 14:48:32 +0000

    add logout

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <header class="header">
-      <router-link
-        class="navBar"
-        to="/"
-        style="display: flex; align-items: center; justify-content: center"
-        ><img :src="logo" alt="logo" style="width: 160px"
-      /></router-link>
+      <router-link class="navBar" to="/" style="display: flex; align-items: center; justify-content: center"><img
+          :src="logo" alt="logo" style="width: 160px" /></router-link>
       <div>
         <router-link class="navBar" to="/">Home</router-link>
         <router-link class="navBar" to="/benchmark">Benchmark</router-link>
         <!-- <router-link class="navBar" to="/resource">Database</router-link> -->
+        <Dropdown v-if="userName">
+          <div href="javascript:void(0)">
+            <Icon type="md-person" />
+            {{ userName }}
+            <Icon type="ios-arrow-down"></Icon>
+          </div>
+          <DropdownMenu slot="list">
+            <DropdownItem @click.native="logout">logout</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <router-link v-else class="navBar" to="/login">Login</router-link>
       </div>
     </header>
   </div>
@@ -18,11 +25,40 @@
 
 <script>
 export default {
+
   data() {
     return {
       logo: require("@/assets/logo.jpg"),
+      userName: ''
     };
   },
+  async mounted() {
+    this.loadUserName();
+    window.addEventListener('userName', this.loadUserName);
+   },
+  beforeDestroy() { 
+    window.removeEventListener('userName', this.loadUserName);
+  },
+  methods: {
+    loadUserName(){
+      this.userName = localStorage.getItem('userName');
+    },
+    logout() {
+      console.log('logout');
+      this.$Modal.confirm({
+        title: 'info',
+        content: '<p>confirm to logout</p>',
+        onOk: () => {
+          localStorage.removeItem('userName');
+          location.reload();
+        },
+        onCancel: () => {
+
+        }
+      });
+
+    }
+  }
 };
 </script>
 
