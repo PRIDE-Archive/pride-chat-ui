f46bbcb35750926f7b3287e8e3d6d8b64d3331bf
shabai517 <shabai517@gmail.com>
2023-12-10 16:39:01 +0000

    update upload file logic
    1. only `.zip` is supported
    2. remove upload folder
    3. add upload zip
    4. add upload model for zip file
    5. add upload progress
    6. check file extention
    7. add the hints for upload successfully or failed

--- src/views/Database.vue
+++ src/views/Database.vue
@@ -5,18 +5,7 @@
 
       <div style="display: flex; align-items: center;justify-content: space-between; ">
         <div style="text-align: left; padding: 8px 16px; display:flex; ">
-          <Upload ref="upload" v-model="files" :action="action" :headers="headers" :show-upload-list="false"
-            :format="['md']" accept="*.md" name="files" multiple :webkitdirectory="true" :before-upload="onBeforeUpload"
-            :on-format-error="handleFormatError" :on-success="onUploadSuccess" :on-error="onUploadFailed"
-            :on-progress="test">
-            <Button icon="ios-cloud-upload-outline">Upload Folder</Button>
-          </Upload>
-          <Upload ref="upload" v-model="files" style="margin-left:8px;" :action="action" :headers="headers"
-            :show-upload-list="false" :format="['md']" accept="*.md" name="files" :webkitdirectory="false"
-            :before-upload="onBeforeUpload" :on-format-error="handleFormatError" :on-success="onUploadSuccess"
-            :on-error="onUploadFailed" :on-progress="test">
-            <Button icon="ios-cloud-upload-outline">Upload File</Button>
-          </Upload>
+          <Button @click="onUploadFile" icon="ios-cloud-upload-outline" type="success">Upload File</Button>
           <Button style="margin-left:8px;" @click="onDeleteAll" type="warning">Delete All</Button>
         </div>
         <div>
@@ -41,11 +30,38 @@
         </Table>
       </div>
     </div>
+    <Modal v-model="uploadModel" title="Upload File" ok-text="close" cancel-text="" :closable="false"
+      :mask-closable="false">
+      <div v-if="uploadStatus == 2">
+        <Alert type="success">Upload Successfully</Alert>
+      </div>
+      <div v-else-if="uploadStatus == 3">
+        <Alert type="error">Upload Failed</Alert>
+      </div>
+      <Upload v-else ref="upload" type="drag" v-model="files" :action="action" :headers="headers"
+        :show-upload-list="false" :format="exts" name="files" :webkitdirectory="false" :before-upload="onBeforeUpload"
+        :on-format-error="handleFormatError" :on-success="onUploadSuccess" :on-error="onUploadFailed"
+        :on-progress="onProgress">
+        <div style="padding: 20px 0" v-if="uploadStatus == 0">
+          <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+          <p>Click or drag files here to upload</p>
+        </div>
+        <div style="padding: 20px 0" v-if="uploadStatus == 1">
+          <p style="margin-top:8px;"><Progress style="width: 100%;" :percent="percent" /></p>
+        </div>
+
+      </Upload>
+
+      <div slot="footer">
+        <Button type="info" :loading="uploadStatus == 1" @click="uploadModel = false">Close</Button>
+      </div>
+
+    </Modal>
   </div>
 </template>
 
 <script>
-import { listFile, deleteFile, donwloadFile, clearFiles } from "@/api/api";
+import { listFile, deleteFile, clearFiles } from "@/api/api";
 import { baseURL, resourceURL } from "@/api/request";
 import navbar from "./Navbar.vue";
 export default {
@@ -84,6 +100,10 @@ export default {
       baseURL: baseURL,
       resourceURL: resourceURL,
       headers: { "Access-Control-Allow-Origin": "*" },
+      percent: 0,
+      uploadModel: false,
+      exts: ['zip'],
+      uploadStatus: 0, // 0 init, 1 uploading, 2 success, 3 error
     };
   },
   components: {
@@ -151,9 +171,30 @@ export default {
       });
 
     },
+    getExtension: function (fileName) {
+      const arr = fileName.split('.');
+      return arr[arr.length - 1];
+    },
+    onUploadFile: function () {
+      this.uploadStatus = 0;
+      this.percent = 0;
+      this.uploadModel = true;
+    },
     onBeforeUpload: function (e) {
-      console.log('onBeforeUpload', e);
-      console.log('files:', this.$refs.upload.fileList);
+      this.percent = 0;
+      let name = e.name;
+      let ext = this.getExtension(name);
+      console.log(name, ext, !this.exts.includes(ext));
+      if (!this.exts.includes(ext)) {
+        console.log("failed");
+        this.$Notice.warning({
+          title: 'The file format is incorrect',
+          desc: 'File format must of ' + this.exts.join(',') + ' it\'s ignored automatically'
+        });
+        return false;
+      }
+      this.uploadStatus = 1;
+      return true;
     },
     handleFormatError(file) {
       this.$Notice.warning({
@@ -166,14 +207,17 @@ export default {
       console.log('files:', this.$refs.upload.fileList);
       let res = await listFile();
       this.list = res.data;
-      this.$Message.success("Upload Successfully");
+      // this.$Message.success("Upload Successfully");
+      this.uploadStatus = 2;
     },
     onUploadFailed: function (e) {
       console.error(e);
-      this.$Message.warning("Upload Failed");
+      // this.$Message.warning("Upload Failed");
+      this.uploadStatus = 3;
     },
-    test(event, file, fileList) {
-      // console.log('aaa',fileList)
+    onProgress(event, file, fileList) {
+      console.log(event, file, fileList)
+      this.percent = event.percent;
     }
   },
 };
