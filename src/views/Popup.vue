e277ad64cbeb7674d158f517c5d01ddf85504ad7
shabai517 <shabai517@gmail.com>
2024-03-29 06:01:20 +0000

    add disclaimer

--- src/views/Popup.vue
+++ src/views/Popup.vue
@@ -5,6 +5,23 @@
             <div class="popup-header">
                 <Icon type="ios-arrow-back" color="white" size="30" @click="isWelcome = true" />
             </div>
+            <Alert banner closable type="warning">
+                <div style="color: gray; text-align:justify; font-size: 12px;">
+                    Disclaimer: This chatbot is designed to assist you with general
+                    information and basic inquiries. Please note that it is not a
+                    replacement for our dedicated
+                    <a href="mailto:pride-support@ebi.ac.uk">helpdesk</a> support or
+                    official
+                    <a href="https://www.ebi.ac.uk/pride/markdownpage/documentationpage">documentation</a>. While the
+                    chatbot is
+                    powered by a language model, it may
+                    occasionally produce responses that are unrelated or incorrect, known
+                    as LLM hallucinations, due to the inherent limitations of the
+                    technology. Therefore, we recommend using this tool as a supplementary
+                    resource and encourage you to consult our helpdesk and official
+                    documentation for critical or complex inquiries.
+                </div>
+            </Alert>
             <Tabs :value="type" :animated="true">
                 <TabPane label="PRIDE Assistant" name="assistant">
                     <chat></chat>
@@ -16,6 +33,7 @@
                 <!-- <Button @click="exportData" size="small" slot="extra">Export Data</Button> -->
             </Tabs>
         </div>
+
     </div>
 </template>
 
@@ -57,7 +75,6 @@ export default {
 </script>
 
 <style scoped>
-
 .popup-header {
     height: 50px;
     background-color: #5BC0BE;
@@ -68,11 +85,11 @@ export default {
     cursor: pointer;
 }
 
- /deep/ .ivu-tabs-nav .ivu-tabs-tab-active{
+/deep/ .ivu-tabs-nav .ivu-tabs-tab-active {
     color: #5BC0BE !important;
 }
-/deep/ .ivu-tabs-ink-bar{
+
+/deep/ .ivu-tabs-ink-bar {
     background-color: #5BC0BE !important;
 }
-
 </style>
